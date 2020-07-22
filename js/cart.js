8eb6eb0ec29dc18d8994df447f7ccfdb26660f73
tschelovek <az18rg@gmail.com>
2020-07-22 14:22:44 +0300

    scripts added html edited

--- js/cart.js
+++ js/cart.js
@@ -1,2 +1,12 @@
 "use strict"
 
+$(document).ready(function(){
+    $('.posterContainer').slick({
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+});
+});
--- js/shop-items.js
+++ js/shop-items.js
@@ -1,2 +1,38 @@
 "use strict"
 
+let requestURL = 'json/items.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'text';
+request.send();
+let allItems;
+
+request.onload = function() {
+    let allItemsText = request.response;
+        allItems = JSON.parse(allItemsText);
+}
+
+let templateProductItem = document.getElementById('templateProductItem').innerHTML,
+    compiled = _.template(templateProductItem),
+    html = '';
+
+function getDataForProductItemTemplate(allItems) {
+    return {
+        id: allItems.id,
+        price: allItems.title,
+        name: allItems.description,
+        img: allItems.available
+    }
+}
+$('#allItemsText').append(html);
+
+allItems.forEach(function(item) {
+    let data = {
+        id: product.id,
+        title: product.title,
+        description: product.description,
+        available: product.available,
+        rating: product.rating
+    }
+    html += compiled(data);
+});
